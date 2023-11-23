45ce1c60ebbdfcfd40b7eaea6b3c97a3348b7f68
cathaloruaidh <cathalormond@gmail.com>
2023-11-23 11:20:37 +0000

    Created the module to generate the posteriors and plots. Added the import to the main script

---
+++ src/Posterior.py
@@ -0,0 +1,14 @@
+import csv
+import logging
+import math
+import os
+import re
+import sys
+
+import numpy as np
+import matplotlib as plt
+import scipy.special as sp
+
+
+# main function
+def PO_main(args):
